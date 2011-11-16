6c1b9dffd4748ac0158b715dde002c3d70d9e60e
ryan <ryan.64bit@gmail.com>
2011-11-16 21:46:59 +0700

    db

--- GPXFileWriterMain/src/id/demo/GPXWrite.java
+++ GPXFileWriterMain/src/id/demo/GPXWrite.java
@@ -1,13 +1,22 @@
 package id.demo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import org.omg.CORBA.Environment;
+
 public class GPXWrite {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//comment
+		//File root = Environment.getExternalStorageDirectory();
+		 
+		// File gpxfile = new File(root, "gpxfile.gpx");
+		 //FileWriter gpxwriter = new FileWriter(gpxfile);
+		 //BufferedWriter out = new BufferedWriter(gpxwriter);
+		 //out.write("Hello world");
 	}
 
 }
